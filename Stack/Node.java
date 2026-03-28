d51a44985fc1c339428292227fd031f80383488d
Ridwan Islam <ridwan.r.islam@gmail.com>
2026-03-28 19:26:27 +0000

    Stack_Task

---
+++ Stack/Node.java
@@ -0,0 +1,13 @@
+package Stack;
+
+public class Node {
+    int elem;
+    Node next;
+
+    // DO NOT CHANGE THIS CONSTRUCTOR
+    public Node(int elem, Node next) {
+        this.elem = elem;
+        this.next = next;
+    }
+
+}
