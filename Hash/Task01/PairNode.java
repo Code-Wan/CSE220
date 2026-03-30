3f78afd6f232c6a25a5b90f85199b63613147094
Ridwan Islam <ridwan.r.islam@gmail.com>
2026-03-30 17:51:33 +0000

    Hashtable: Deletion Operation :

---
+++ Hash/Task01/PairNode.java
@@ -0,0 +1,16 @@
+package Hash.Task01;
+
+public class PairNode {
+
+    Integer key;
+    String value;
+    PairNode next;
+
+    // YOU HAVE TO WRITE THIS CONSTRUCTOR BUT NO NEED TO SUBMIT IT
+    public PairNode(Integer k, String v) {
+        this.key = k;
+        this.value = v;
+        this.next = null;
+    }
+
+}
